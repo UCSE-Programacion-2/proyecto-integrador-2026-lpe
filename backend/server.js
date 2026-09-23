57b83617e4b6e20caac11b9b4c50ce6c5eae5ec5
Marco Castillo Cabrera <marco.cast.cab@gmail.com>
2026-09-23 00:10:46 -0300

    agregado plantilla de login y registro. Uso de api para consutlar en DB inicio de sesion.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const productoRoutes = require('./routes/productoRoutes');
@@ -10,12 +11,13 @@ const authRoutes = require('./routes/authRoutes');
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 
+app.use(cors());
+app.use(express.json());
 
 conectarDB();
 
-app.use('/productos', productoRoutes);
+app.use('/api/productos', productoRoutes);
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
